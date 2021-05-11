f761b4cc8c860c3b73401d8d6cbed57f0f553512
Olympicene <kevinjcord@gmail.com>
2021-05-11 12:27:07 -0500

    ran prettier on everything

--- database/config.js
+++ database/config.js
@@ -1,11 +1,10 @@
-
 //time in between commands
-exports.timeout_milliseconds = 1000
+exports.timeout_milliseconds = 1000;
 
 //threadIDs of groupchats/chats its allowed in
 exports.allowed_threads = [
-    '2401681243197992', 
-    '4432056806822983', 
-    '4341136652627262', 
-    '4258360417509656'
-]
+  "2401681243197992",
+  "4432056806822983",
+  "4341136652627262",
+  "4258360417509656",
+];
--- index.js
+++ index.js
@@ -1,59 +1,63 @@
-const path = require('path');
-const response = require('./database/response.json');
-const config = require('./database/config')
+const path = require("path");
+const response = require("./database/response.json");
+const config = require("./database/config");
 const fs = require("fs");
-const Timeout = require('./src/Timeout');
-const login = require('facebook-chat-api');
-
+const Timeout = require("./src/Timeout");
+const login = require("facebook-chat-api");
 
 //database with credentials & responses
-const databaseDir = path.resolve(__dirname + '/database/'); 
-
+const databaseDir = path.resolve(__dirname + "/database/");
 
 ////////////////////////////////////////////////////LOGIN////////////////////////////////////////////////////
-login({appState: JSON.parse(fs.readFileSync('database/appstate.json', 'utf8'))}, (err, api) => {
-    if(err) return console.error(err);
+login(
+  { appState: JSON.parse(fs.readFileSync("database/appstate.json", "utf8")) },
+  (err, api) => {
+    if (err) return console.error(err);
 
     //store most recent cookie in database
-    fs.writeFileSync(databaseDir + '/appstate.json', JSON.stringify(api.getAppState())); 
+    fs.writeFileSync(
+      databaseDir + "/appstate.json",
+      JSON.stringify(api.getAppState())
+    );
 
     api.setOptions({
-        listenEvents: true,
-        selfListen: true,
-        forceLogin: true,
-    })
+      listenEvents: true,
+      selfListen: true, // turn this off if you dont want the bot to respond to itself
+      forceLogin: true,
+    });
 
     //keeps track of which thread is in timout
     //default is 1000 (1 seconds)
-    var use = new Timeout(config.timeout_milliseconds); 
+    var use = new Timeout(config.timeout_milliseconds);
 
     //stores the threadIDs that the bot is allowed to respond to
     threadIDs = config.allowed_threads;
 
-
-////////////////////////////////////////////////////LISTEN////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////LISTEN////////////////////////////////////////////////////
     api.listenMqtt((err, event) => {
-        if(err) return console.error(err);
+      if (err) return console.error(err);
 
-        //DEBUG
-        // console.log(event);
+      //DEBUG
+      // console.log(event);
 
-        //check if thread id is valid and not in timeout
+      //check if thread id is valid and not in timeout
 
-        if(threadIDs.includes(event.threadID)  && !use.inTimeout(event.threadID)) { 
-            if(event.body in response) {
+      if (
+        threadIDs.includes(event.threadID) &&
+        !use.inTimeout(event.threadID)
+      ) {
+        if (event.body in response) {
+          //gets message from response.json
+          var message = response[event.body];
 
-                //gets message from response.json
-                var message = response[event.body];
+          //sends message and puts thread in timeout afterward
+          api.sendMessage(message, event.threadID, (err) => {
+            if (err) return console.error(err);
 
-                //sends message and puts thread in timeout afterward
-                api.sendMessage(message, event.threadID, (err) => { 
-                    if(err) return console.error(err);
-                    
-                    use.threadTimeout(event.threadID);
-                });
-            }
+            use.threadTimeout(event.threadID);
+          });
         }
+      }
     });
-});
-
+  }
+);
